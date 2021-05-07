f3135bd228a7144712c60f0530c1578360f7a692
Simon Gombrii <simon@gombrii.se>
2021-05-07 21:20:26 +0200

    Adjust gravity constant

--- src/Logic/Component/GravityComponent.java
+++ src/Logic/Component/GravityComponent.java
@@ -5,7 +5,7 @@ import Logic.Entity;
 
 public class GravityComponent implements EntityComponent{
 
-    private static final double GRAVITY = 0.3;
+    private static final double GRAVITY = 0.4;
 
     public void apply(Entity owner){
         owner.addVelocity(0, GRAVITY);
